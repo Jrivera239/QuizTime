54aa4a8e8001cde00a7f1f78e32f0ed7bfde90d4
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-07-06 19:05:53 -0400

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+const quizDisplay = document.getElementById("display");
+let timeLeft = document.querySelector(".time-left");
+let qizConstainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector("number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer= document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+
+
