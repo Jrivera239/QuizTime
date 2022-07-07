060047defe06d6a259f76b8ca178021ba3a39021
Joel Rivera <106676739+Jrivera239@users.noreply.github.com>
2022-07-07 07:49:09 -0400

    working

--- script.js
+++ script.js
@@ -1,116 +1,174 @@
-const quizDisplay = document.getElementById("display");
+//References//
 let timeLeft = document.querySelector(".time-left");
-let qizConstainer = document.getElementById("container");
+let quizContainer = document.getElementById("container");
 let nextBtn = document.getElementById("next-button");
-let countOfQuestion = document.querySelector("number-of-question");
+let countOfQuestion = document.querySelector(".number-of-question");
 let displayContainer = document.getElementById("display-container");
-let scoreContainer= document.querySelector(".score-container");
+let scoreContainer = document.querySelector(".score-container");
 let restart = document.getElementById("restart");
-let userScore = document.getElementById("User-Score");
-let startScreen = document.getElementById ("start-screen");
-let startButton = document.getElementById ("start-button");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
 let questionCount;
 let scoreCount = 0;
 let count = 15;
 let countdown;
 
+//Questions and Options array//
 
-// Options array and questions//
-
-const quizArray = [{id: "0",
- question:"If the mass of the sun is about 333,000 times the mass of the Earth.", options:["Earth is smaller","Earth is bigger","Sun is bigger","Sun is smaller"], correct:"Sun is bigger"
-},
-{
+const quizArray = [
+  {
     id: "1",
+    question: "If the mass of the sun is about 333,000 times the mass of the Earth.", options:["Earth is smaller","Earth is bigger","Sun is bigger","Sun is smaller"], correct:"Sun is bigger"
+  },
+  {
+    id: "2",
  question:"What is black outside and yellow inside?" ,
  options:["Tennis Ball","Avocado","Bowl","Ninja Chick"], correct:"Ninja Chick"
 },
 {
-    id: "2",
+    id: "3",
  question:"What happened to Dinosaurs?" ,
  options:["They playing hide and seek", "They were extinct","Starve to death","T-Rex ate them all"], correct:"They were extint",
 },
 ];
 
-//Time//
-const timerDisplay = () => {
-    countdown = setInterval (() => {
-        count--;
-        timeLeft.innerHTML = `${count}s`;
-        if (count == 0){
-            clearInterval(countdown);
-            displayNext();
-        }
-
-    }, 1000);
-}
-
-
-//    //
-const quizDisplay =(questionCount) => {
-    let quizDisplay = document.querySelectorAll(".conainer_mid");
-// Hiding Cards//
-quizCards.forEach((card) => {
-    card.classList.add ("hide");
-
+//Restart Quiz//
+restart.addEventListener("click", () => {
+  initial();
+  displayContainer.classList.remove("hide");
+  scoreContainer.classList.add("hide");
 });
 
-quizCards[questionCount].classList.remove("hide");
-
-}
-
-
-//Creation//
-
-function quizCreator(){
-
-    quizArray.sort(()=> Math.random() -0.5);
-    // //
-    for(let i of quizArray){
-        // //
-        i.options.sort(() => Math.random() -0.5);
-        // //
-        let div = document.createElement("div");
-        div.classList.add("contaner-mid"; "hide");
-        // //
-        countOfQuestion.innerHTML = 1+ " of "+ quizArray.length+" Question ";
-        // //
-        let question_DIV = document.createElement("p");
-        question_DIV.classList.add("question");
-        question_DIV.innerHTML = i.question;
-        // // 
-        div.apprenChild(question_DIV);
-
-        div.innerHTML += `
-        <button class= "option-div" onclick="checker (this)">${i.options[0]}</button>
-        <button class= "option-div" onclick="checker (this)">${i.options[1]}</button>
-        <button class= "option-div" onclick="checker (this)">${i.options[2]}</button>
-        <button class= "option-div" onclick="checker (this)">${i.options[4]}</button>
-        `;
-        quizContainer.apprenChild(div);
+//Next Button//
+nextBtn.addEventListener(
+  "click",
+  (displayNext = () => {
+    //increment questionCount//
+    questionCount += 1;
+    //if last question//
+    if (questionCount == quizArray.length) {
+      //hide question container and display score//
+      displayContainer.classList.add("hide");
+      scoreContainer.classList.remove("hide");
+      //user score//
+      userScore.innerHTML =
+        "Your score is " + scoreCount + " out of " + questionCount;
+    } else {
+      //display questionCount//
+      countOfQuestion.innerHTML =
+        questionCount + 1 + " of " + quizArray.length + " Question";
+      //display quiz//
+      quizDisplay(questionCount);
+      count = 15;
+      clearInterval(countdown);
+      timerDisplay();
     }
-}
-// //
-function initial (){
-    quizContainer.innerHTML = "";
-    questionCount = 0;
-    scoreCount = 0;
-    count = 15;
-    quizCreator();
-    quizDisplay(questionCount);
+  })
+);
+
+//Timer//
+const timerDisplay = () => {
+  countdown = setInterval(() => {
+    count--;
+    timeLeft.innerHTML = `${count}s`;
+    if (count == 0) {
+      clearInterval(countdown);
+      displayNext();
+    }
+  }, 1000);
+};
+
+//Display quiz//
+const quizDisplay = (questionCount) => {
+  let quizCards = document.querySelectorAll(".container-mid");
+  //Hide other cards//
+  quizCards.forEach((card) => {
+    card.classList.add("hide");
+  });
+  //display current question card//
+  quizCards[questionCount].classList.remove("hide");
+};
+
+//Quiz Creation//
+function quizCreator() {
+  //randomly sort questions//
+  quizArray.sort(() => Math.random() - 0.5);
+  //generate quiz//
+  for (let i of quizArray) {
+    //randomly sort options//
+    i.options.sort(() => Math.random() - 0.5);
+    //quiz card creation//
+    let div = document.createElement("div");
+    div.classList.add("container-mid", "hide");
+    //question number//
+    countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+    //question//
+    let question_DIV = document.createElement("p");
+    question_DIV.classList.add("question");
+    question_DIV.innerHTML = i.question;
+    div.appendChild(question_DIV);
+    //options//
+    div.innerHTML += `
+    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
+     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
+      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
+       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
+    `;
+    quizContainer.appendChild(div);
+  }
 }
 
-// //
-startButton.addEventListener("click",() => {
-    startScreen.classList.add("hide");
-    startScreen.classList.remove 
-    initial ();
-}
-// hiding quiz and display//
+//Checker Function to check if option is correct or not//
+function checker(userOption) {
+  let userSolution = userOption.innerText;
+  let question =
+    document.getElementsByClassName("container-mid")[questionCount];
+  let options = question.querySelectorAll(".option-div");
 
-window.onload = () => {
-    
-    displayContainer.classList.add ('hide');
- 
+  //if user clicked answer == correct option stored in object//
+  if (userSolution === quizArray[questionCount].correct) {
+    userOption.classList.add("correct");
+    scoreCount++;
+  } else {
+    userOption.classList.add("incorrect");
+    //For marking the correct option//
+    options.forEach((element) => {
+      if (element.innerText == quizArray[questionCount].correct) {
+        element.classList.add("correct");
+      }
+    });
+  }
+
+  //clear interval(stop timer)//
+  clearInterval(countdown);
+  //disable all options//
+  options.forEach((element) => {
+    element.disabled = true;
+  });
+}
+
+//initial setup//
+function initial() {
+  quizContainer.innerHTML = "";
+  questionCount = 0;
+  scoreCount = 0;
+  count = 15;
+  clearInterval(countdown);
+  timerDisplay();
+  quizCreator();
+  quizDisplay(questionCount);
+}
+
+//when user click on start button//
+startButton.addEventListener("click", () => {
+  startScreen.classList.add("hide");
+  displayContainer.classList.remove("hide");
+  initial();
 });
 
+//hide quiz and display start screen//
+window.onload = () => {
+  startScreen.classList.remove("hide");
+  displayContainer.classList.add("hide");
+};
